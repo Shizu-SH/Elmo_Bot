06b19036bea6680f501db200cef6e1023d973204
Shizu-SH <119007576+Shizu-SH@users.noreply.github.com>
2023-07-28 15:59:55 +0100

    Update gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -4,9 +4,9 @@ let group = m.chat
 conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
-body: '𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})   
+body: 'Elmo - 𝙱𝚘𝚝',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu.png"),
+sourceUrl: `https://github.com/Shizu-SH/Elmo_Bot`}}})   
 }
 handler.help = ['linkgroup']
 handler.tags = ['group']
