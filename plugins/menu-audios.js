7729889c65d70069bbeeac6ffc3ca0b8a0a8b482
Shizu-SH <119007576+Shizu-SH@users.noreply.github.com>
2023-07-30 11:56:33 +0100

    Update menu-audios.js

--- plugins/menu-audios.js
+++ plugins/menu-audios.js
@@ -29,16 +29,154 @@ let str = `╭═══〘 ✯✯✯✯✯✯✯✯✯ 〙══╮
 ┏━━━━━━━━━━━━━━━━┓
 ┃ *<𝐌𝐄𝐍𝐔 𝐀𝐔𝐃𝐈𝐎𝐒/>*
 ┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
-┣*- 𝙴𝚂𝙲𝚁𝙸𝙱𝙴 𝙻𝙰𝚂 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴𝚂 𝙿𝙰𝙻𝙰𝙱𝚁𝙰𝚂 𝙾 𝙵𝚁𝙰𝚂𝙴𝚂 𝚂𝙸𝙽 𝙽𝙸𝙽𝙶𝚄𝙽 𝙿𝚁𝙴𝙵𝙸𝙹𝙾 (#, /, *, .)*
-┣ ඬ⃟🔊 _awebo_
-┣ ඬ⃟🔊 _elmo sabe donde vives_
-┣ ඬ⃟🔊 _droga_
-┣ ඬ⃟🔊 _chupa chupa_
-┣ ඬ⃟🔊 _cariñosas_
-┣ ඬ⃟🔊 _te revelas contra elmo?_
-┣ ඬ⃟🔊 _que dijistes_
-┣ ඬ⃟🔊 _te crees mejor que Elmo_
-┣ ඬ⃟🔊 _Elmo es chingon_
+*- 𝙴𝚂𝙲𝚁𝙸𝙱𝙴 𝙻𝙰𝚂 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴𝚂 𝙿𝙰𝙻𝙰𝙱𝚁𝙰𝚂 𝙾 𝙵𝚁𝙰𝚂𝙴𝚂 𝚂𝙸𝙽 𝙽𝙸𝙽𝙶𝚄𝙽 𝙿𝚁𝙴𝙵𝙸𝙹𝙾 (#, /, *, .)*
+∘ _awebo_
+∘ _elmo sabe donde vives_
+∘ _droga_
+∘ _chupa chupa_
+∘ _cariñosas_
+∘ _te revelas contra elmo?_
+∘ _que dijistes_
+∘ _te crees mejor que Elmo_
+∘ _Elmo es chingon_
+∘ _Noche de paz_
+∘ _Buenos dias_
+∘ _Audio hentai_
+∘ _Fiesta del admin_
+∘ _Fiesta del admin 2_
+∘ _Fiesta del administrador_ 
+∘ _Viernes_
+∘ _Mierda de Bot_
+∘ _Me olvidé_
+∘ _Baneado_
+∘ _Feliz navidad_
+∘ _A nadie le importa_
+∘ _Sexo_
+∘ _Vete a la vrg_
+∘ _Ara ara_
+∘ _Hola_
+∘ _Un pato_
+∘ _Nyanpasu_
+∘ _Te amo_
+∘ _Yamete_
+∘ _Te diagnostico con gay_
+∘ _Quien es tu sempai botsito 7w7_
+∘ _Bañate_
+∘ _Vivan los novios_
+∘ _Marica quien_
+∘ _Es puto_
+∘ _La biblia_
+∘ _Onichan_
+∘ _Bot puto_
+∘ _Feliz cumpleaños_
+∘ _Pasa pack Bot_
+∘ _Atencion grupo_
+∘ _Homero chino_
+∘ _Oh me vengo_
+∘ _Murio el grupo_
+∘ _Siuuu_
+∘ _Rawr_
+∘ _UwU_
+∘ _:c_
+∘ _a_
+∘ _Hey_
+∘ _Enojado_
+∘ _Enojada_
+∘ _Chao_
+∘ _Hentai_
+∘ _Triste_
+∘ _Estoy triste_
+∘ _Me pican los cocos_
+∘ _Contexto_
+∘ _Me voy_
+∘ _Tengo los calzones del admin_
+∘ _Entrada épica_ 
+∘ _Esto va ser épico papus_
+∘ _Ingresa épicamente_
+∘ _Bv_
+∘ _Yoshi_
+∘ _No digas eso papu_
+∘ _Ma ma masivo_
+∘ _Masivo_
+∘ _Basado_
+∘ _Basada_
+∘ _Fino señores_
+∘ _Verdad que te engañe_
+∘ _Sus_
+∘ _Ohayo_
+∘ _La voz de hombre_
+∘ _Pero esto_
+∘ _Bien pensado Woody_
+∘ _Jesucristo_
+∘ _Wtf_
+∘ _Una pregunta_
+∘ _Que sucede_
+∘ _Hablame_
+∘ _Pikachu_
+∘ _Niconico_
+∘ _Yokese_
+∘ _Omaiga_
+∘ _Nadie te preguntó_
+∘ _Bueno si_
+∘ _Usted está detenido_
+∘ _No me hables_
+∘ _No chu_
+∘ _El pepe_
+∘ _Pokémon_
+∘ _No me hagas usar esto_
+∘ _Esto va para ti_
+∘ _Abduzcan_
+∘ _Joder_
+∘ _Hablar primos_
+∘ _Mmm_
+∘ _Orale_
+∘ _Me anda buscando anonymous_
+∘ _Blackpink in your area_
+∘ _Cambiate a Movistar_
+∘ _Momento equisde | Momento XD_
+∘ _Todo bien | 😇_
+∘ _Te gusta el Pepino | 🥒_
+∘ _El tóxico_
+∘ _Moshi moshi_
+∘ _Calla Fan de BTS_
+∘ _Que tal grupo_
+∘ _Muchachos_
+∘ _Está Zzzz | 😴_
+∘ _Goku Pervertido_
+∘ _Potaxio | 🥑_
+∘ _Nico nico_
+∘ _El rap de Fernanfloo_
+∘ _Tal vez_
+∘ _Corte corte_
+∘ _Buenas noches_
+∘ _Porque ta tite_
+∘ _Eres Fuerte_
+∘ _Bueno Master | 🫂_
+∘ _No Rompas más_
+∘ _Traiganle una falda_
+∘ _Se están riendo de mí_
+∘ _Su nivel de pendejo_
+∘ _Bienvenido/a 🥳 | 👋_
+∘ _Elmo sabe donde vives_
+∘ _tunometecabrasaramambiche_
+∘ _Y este quien es_
+∘ _Motivación_
+∘ _En caso de una investigación_
+∘ _Buen día grupo | 🙌_
+∘ _Las reglas del grupo_
+∘ _Oye | 🐔_
+∘ _Ig de la minita_
+∘ _Gaspi frase_
+∘ _Vamos!!_
+∘ _Se pudrio_
+∘ _Gol!_
+
+╭════• ೋ•✧๑♡๑✧•ೋ •════╮
+                      🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈
+         0:40 ━❍──────── -8:39
+         ↻     ⊲  Ⅱ  ⊳     ↺
+         VOLUMEN: ▁▂▃▄▅▆▇ 100%
+╰════• ೋ•✧๑♡๑✧•ೋ •════╯
 ┗━━━━━━━━━━━━━━━━┛`.trim()
 if (m.isGroup) {
 //await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
