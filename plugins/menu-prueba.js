6a379ba55d81ceebef2073bce937a14de4035647
Shizu-SH <119007576+Shizu-SH@users.noreply.github.com>
2023-07-30 17:06:31 +0100

    Update menu-prueba.js

--- plugins/menu-prueba.js
+++ plugins/menu-prueba.js
@@ -20,7 +20,7 @@ let readMore = more.repeat(850)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
 var document = doc[Math.floor(Math.random() * doc.length)]    
-let str = `  *╭━━❍Eʅɱσ - Bσƚ❍━━╮* 
+let str = `*╭━━❍Eʅɱσ - Bσƚ❍━━╮* 
  *┃ ╭━━━━━━━━━━━━━━━━╮* 
  *┃ ┃ ╭┈────────────╮* 
  *┃ ┃ │❍ 🅼🅴🅽🆄 ❍* 
