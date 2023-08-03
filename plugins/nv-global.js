9bd8ce7bf610c27aa51a65f6bd1eea966a818f4e
Shizu-SH <119007576+Shizu-SH@users.noreply.github.com>
2023-08-03 21:08:41 +0100

    Update nv-global.js

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -12,6 +12,11 @@ let vn = './media/awebo.m4a'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })}    
 
+if (!chat.isBanned && chat.audios && m.text.match(/(callate la boca|no estoy hablando|silencio)/gi)) {
+let vn = './media/callate la boca.opus'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })}
+
 if (!chat.isBanned && chat.audios && m.text.match(/(chupa)/gi)) {
 let vn = './media/chupa.m4a'
 this.sendPresenceUpdate('recording', m.chat)   
