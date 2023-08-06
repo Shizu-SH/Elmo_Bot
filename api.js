463305479f79a09ff566f7e21b03e15bb5767247
Shizu-SH <119007576+Shizu-SH@users.noreply.github.com>
2023-08-06 13:19:34 +0100

    Update api.js

--- api.js
+++ api.js
@@ -9,7 +9,7 @@ import moment from 'moment-timezone';
 
 /* si quieres modificar esto y agregarr mas apis asegurate poner global.tuapi = ['apikey']  skid 🤑 */
 
-global.openai_key = 'sk-0';
+global.openai_key = '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';
 /* Consigue tu ApiKey en este enlace: https://platform.openai.com/account/api-keys */
 
 global.openai_org_id = 'org-3';
