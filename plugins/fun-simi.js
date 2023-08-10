3c6312ce5ad09f983a3653774ed497d775d392e6
Shizu-SH <119007576+Shizu-SH@users.noreply.github.com>
2023-08-10 15:41:29 +0100

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,34 +1,30 @@
-import fetch from "node-fetch"
-let handler = async (m, { conn, text }) => {  
-let sistema1 = await fetch(`https://raw.githubusercontent.com/Skidy89/chat-gpt-jailbreak/main/Text.txt`).then(v => v.text())
-
-async function getOpenAIChatCompletion(texto) {
-  const openaiAPIKey = global.openai_key
-  let chgptdb = global.chatgpt.data.users[m.sender]
-  chgptdb.push({ role: 'user', content: texto })
-  const url = "https://api.openai.com/v1/chat/completions"
-  const headers = { "Content-Type": "application/json", "Authorization": `Bearer ${openaiAPIKey}` };
-  const data = { "model": "gpt-3.5-turbo", "messages": [{ "role": "system", "content": sistema1 }, ...chgptdb, ]};
+import translate from '@vitalets/google-translate-api';
+import fetch from 'node-fetch';
+const handler = async (m, {text, command, args, usedPrefix}) => {
+  if (!text) throw `*[❗] QUE MIERDA QUERES?*\n`;
   try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: headers,
-      body: JSON.stringify(data),
-    });
-    if (!response.ok) {
-      throw new Error("Error al obtener respuesta de OpenAI")
+    const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=es');
+    const resSimi = await api.json();
+    m.reply(resSimi.success);
+  } catch {
+    try {
+      if (text.includes('Hola')) text = text.replace('Hola', 'Hello');
+      if (text.includes('hola')) text = text.replace('hola', 'Hello');
+      if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO');
+      const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + text);
+      const resu = await reis.json();
+      const nama = m.pushName || '1';
+      const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
+      const res = await api.json();
+      const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
+      const resu2 = await reis2.json();
+      m.reply(resu2[0][0][0]);
+    } catch {
+      throw `*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙴 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*`;
     }
-    const result = await response.json()
-    const finalResponse = result.choices[0].message.content
-    return finalResponse
-  } catch (error) {
-    console.error("Error, vuelva a intentarlo:", error)
-    return null;
   }
-}
-
-let respuesta = await getOpenAIChatCompletion(text)
-m.reply(respuesta)
-}
-handler.command = /^(bot|elmo|simsimi|simisimi)$/i
-export default handler
+};
+handler.help = ['simi', 'bot'].map((v) => v + ' <teks>');
+handler.tags = ['fun'];
+handler.command = /^((sim)?simi|bot|alexa|cortana)$/i;
+export default handler;
