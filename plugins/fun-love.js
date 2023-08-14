c88754edebd1ab75bc797d457e826087beafdecb
Shizu-SH <119007576+Shizu-SH@users.noreply.github.com>
2023-08-14 12:25:29 +0100

    Update fun-love.js

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,27 +1,30 @@
-const handler = async (m, {conn, command, text}) => {
-  const love = `*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
-*El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
-*Deberias pedirle que sea tu  novia/o ?*
-`.trim();
-  m.reply(love, null, {mentions: conn.parseMention(love)});
+const loading = async (conn, m) => {
+    var hawemod = [
+        "《 █▒▒▒▒▒▒▒▒▒▒▒》10%",
+        "《 ████▒▒▒▒▒▒▒▒》30%",
+        "《 ███████▒▒▒▒▒》50%",
+        "《 ██████████▒▒》80%",
+        "《 ████████████》100%",
+        "\n𝙻𝙾𝙰𝙳𝙸𝙽𝙶 𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙴𝙳..."
+    ];
+    let { key } = await conn.sendMessage(m.chat, { text: 'ʟᴏᴀᴅɪɴɢ...' });
+
+    for (let i = 0; i < hawemod.length; i++) {
+        /* puedes agregar un delay */
+        await conn.sendMessage(m.chat, { text: hawemod[i], edit: key });
+    }
 };
+
+const handler = async (m, { conn, command, text }) => {
+    await loading(conn, m); // Mostrar el efecto de carga
+    const love = `*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
+    *El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
+    *Deberias pedirle que sea tu  novia/o ?*
+    `.trim();
+    m.reply(love, null, { mentions: conn.parseMention(love) });
+};
+
 handler.help = ['love'];
 handler.tags = ['fun'];
 handler.command = /^(love)$/i;
 export default handler;
-async function loading () {
-var hawemod = [
-"《 █▒▒▒▒▒▒▒▒▒▒▒》10%",
-"《 ████▒▒▒▒▒▒▒▒》30%",
-"《 ███████▒▒▒▒▒》50%",
-"《 ██████████▒▒》80%",
-"《 ████████████》100%",
-"\n𝙻𝙾𝙰𝙳𝙸𝙽𝙶 𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙴𝙳..."
-]
-let { key } = await conn.sendMessage(m.chat, {text: 'ʟᴏᴀᴅɪɴɢ...'})
-
-for (let i = 0; i < hawemod.length; i++) {
-/* puedes agregar un delay */
-await conn.sendMessage(m.chat, {text: hawemod[i], edit: key });
-}
-}
