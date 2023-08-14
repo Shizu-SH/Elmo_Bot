277f0515bd90f506afe85ea3f4cb05c42f27396b
Shizu-SH <119007576+Shizu-SH@users.noreply.github.com>
2023-08-14 12:18:51 +0100

    Update fun-love.js

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,25 +1,24 @@
-
-quoted = {quoted: {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'}};
-
 let handler = async (m, { conn, command, text }) => {
 let love = `*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
 *El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
 *Deberias pedirle que sea tu  novia/o ?*
 `.trim()
-const { key } = await conn.sendMessage(m.chat, {image: pp, caption: '❤️Calculando Amor❤️', mentions: [m.sender]}, quoted);
-await new Promise(resolve => setTimeout(resolve, 500));
-await conn.sendMessage(m.chat, {image: pp, caption: '▓▓▓▒▒▒▒▒▒▒ 30%', edit: key, mentions: [m.sender]});
-await new Promise(resolve => setTimeout(resolve, 500));
-await conn.sendMessage(m.chat, {image: pp, caption: '▓▓▓▓▓▒▒▒▒▒ 50%', edit: key, mentions: [m.sender]});
-await new Promise(resolve => setTimeout(resolve, 500));
-await conn.sendMessage(m.chat, {image: pp, caption: '▓▓▓▓▓▓▓▒▒▒ 70%', edit: key, mentions: [m.sender]});
-await new Promise(resolve => setTimeout(resolve, 500));
-await conn.sendMessage(m.chat, {image: pp, caption: '▓▓▓▓▓▓▓▓▓▒ 90%', edit: key, mentions: [m.sender]});
-await new Promise(resolve => setTimeout(resolve, 500));
-await conn.sendMessage(m.chat, {image: pp, caption: '▓▓▓▓▓▓▓▓▓▓ 100%', edit: key, mentions: [m.sender]});
-await new Promise(resolve => setTimeout(resolve, 500));
-await conn.sendMessage(m.chat, {image: pp, caption: str, edit: key, mentions: [m.sender]});
-await new Promise(resolve => setTimeout(resolve, 500));
+ async function loading () {
+var hawemod = [
+"《 █▒▒▒▒▒▒▒▒▒▒▒》10%",
+"《 ████▒▒▒▒▒▒▒▒》30%",
+"《 ███████▒▒▒▒▒》50%",
+"《 ██████████▒▒》80%",
+"《 ████████████》100%",
+"\n𝙻𝙾𝙰𝙳𝙸𝙽𝙶 𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙴𝙳..."
+]
+let { key } = await conn.sendMessage(m.chat, {text: 'ʟᴏᴀᴅɪɴɢ...'})
+
+for (let i = 0; i < hawemod.length; i++) {
+/* puedes agregar un delay */
+await conn.sendMessage(m.chat, {text: hawemod[i], edit: key });
+}
+}
 m.reply(love, null, { mentions: conn.parseMention(love) })}
 handler.help = ['love']
 handler.tags = ['fun']
