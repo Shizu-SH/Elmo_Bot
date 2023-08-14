de1bb2079c04228d5ccb4b12bff41091070bc065
Shizu-SH <119007576+Shizu-SH@users.noreply.github.com>
2023-08-14 12:20:42 +0100

    Update fun-love.js

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,9 +1,9 @@
-let handler = async (m, { conn, command, text }) => {
-let love = `*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
+const handler = async (m, {conn, command, text}) => {
+  const love = `*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
 *El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
 *Deberias pedirle que sea tu  novia/o ?*
-`.trim()
- async function loading () {
+`.trim();
+  async function loading () {
 var hawemod = [
 "《 █▒▒▒▒▒▒▒▒▒▒▒》10%",
 "《 ████▒▒▒▒▒▒▒▒》30%",
@@ -19,16 +19,9 @@ for (let i = 0; i < hawemod.length; i++) {
 await conn.sendMessage(m.chat, {text: hawemod[i], edit: key });
 }
 }
-m.reply(love, null, { mentions: conn.parseMention(love) })}
-handler.help = ['love']
-handler.tags = ['fun']
-handler.command = /^(love)$/i
+  m.reply(love, null, {mentions: conn.parseMention(love)});
+};
+handler.help = ['love'];
+handler.tags = ['fun'];
+handler.command = /^(love)$/i;
 export default handler;
-
-function clockString(ms) {
-  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
-  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
-  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
-}
-
