12e367c7b81ec4b69ea1d335345289ba4a5204fd
Shizu-SH <119007576+Shizu-SH@users.noreply.github.com>
2023-08-14 12:21:42 +0100

    Update fun-love.js

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -3,7 +3,13 @@ const handler = async (m, {conn, command, text}) => {
 *El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
 *Deberias pedirle que sea tu  novia/o ?*
 `.trim();
-  async function loading () {
+  m.reply(love, null, {mentions: conn.parseMention(love)});
+};
+handler.help = ['love'];
+handler.tags = ['fun'];
+handler.command = /^(love)$/i;
+export default handler;
+async function loading () {
 var hawemod = [
 "《 █▒▒▒▒▒▒▒▒▒▒▒》10%",
 "《 ████▒▒▒▒▒▒▒▒》30%",
@@ -19,9 +25,3 @@ for (let i = 0; i < hawemod.length; i++) {
 await conn.sendMessage(m.chat, {text: hawemod[i], edit: key });
 }
 }
-  m.reply(love, null, {mentions: conn.parseMention(love)});
-};
-handler.help = ['love'];
-handler.tags = ['fun'];
-handler.command = /^(love)$/i;
-export default handler;
