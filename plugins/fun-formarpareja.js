3636de2223181156f86ebf612791cd7cede0c17d
Shizu-SH <119007576+Shizu-SH@users.noreply.github.com>
2023-08-14 12:27:40 +0100

    Update fun-formarpareja.js

--- plugins/fun-formarpareja.js
+++ plugins/fun-formarpareja.js
@@ -1,15 +1,38 @@
 let toM = a => '@' + a.split('@')[0]
-function handler(m, { groupMetadata }) {
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b
-do b = ps.getRandom()
-while (b === a)
-m.reply(`*${toM(a)}, 𝙳𝙴𝙱𝙴𝚁𝙸𝙰𝚂 𝙲𝙰𝚂𝙰𝚁𝚃𝙴 💍 𝙲𝙾𝙽 ${toM(b)}, 𝙷𝙰𝙲𝙴𝙽 𝚄𝙽𝙰 𝙱𝚄𝙴𝙽𝙰 𝙿𝙰𝚁𝙴𝙹𝙰 💓*`, null, {
-mentions: [a, b]
-})}
-handler.help = ['formarpareja']
-handler.tags = ['main', 'fun']
-handler.command = ['formarpareja','formarparejas']
-handler.group = true
-export default handler
+
+const loading = async (conn, m) => {
+    var hawemod = [
+        "Calculando...",
+        "Buscando posibles parejas...",
+        "Analizando compatibilidad...",
+        "Generando resultados...",
+        "Listo para mostrar parejas..."
+    ];
+    let { key } = await conn.sendMessage(m.chat, { text: 'ʟᴏᴀᴅɪɴɢ...' });
+
+    for (let i = 0; i < hawemod.length; i++) {
+        /* puedes agregar un delay */
+        await conn.sendMessage(m.chat, { text: hawemod[i], edit: key });
+    }
+};
+
+function handler(m, { groupMetadata, conn }) {
+    loading(conn, m); // Mostrar el efecto de carga
+
+    let ps = groupMetadata.participants.map(v => v.id);
+    let a = ps.getRandom();
+    let b;
+
+    do b = ps.getRandom();
+    while (b === a);
+
+    m.reply(`*${toM(a)}, 𝙳𝙴𝙱𝙴𝚁𝙸𝙰𝚂 𝙲𝙰𝚂𝙰𝚁𝚃𝙴 💍 𝙲𝙾𝙽 ${toM(b)}, 𝙷𝙰𝙲𝙴𝙽 𝚄𝙽𝙰 𝙱𝚄𝙴𝙽𝙰 𝙿𝙰𝚁𝙴𝙹𝙰 💓*`, null, {
+        mentions: [a, b]
+    });
+}
+
+handler.help = ['formarpareja'];
+handler.tags = ['main', 'fun'];
+handler.command = ['formarpareja', 'formarparejas'];
+handler.group = true;
+export default handler;
